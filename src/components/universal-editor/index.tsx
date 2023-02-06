06bbb5057ef799a2aa020999c8ed4a7d6f1906f0
Sergey Solomin <solomin.s.n@gmail.com>
2023-02-06 22:31:20 +0300

    Added loading state to universal editor component. Used universal editor for "Friendly name" and "Converter Id" fields.

--- src/components/universal-editor/index.tsx
+++ src/components/universal-editor/index.tsx
@@ -1,10 +1,11 @@
-import { FunctionalComponent, h, RefObject, Fragment } from "preact";
-import { forwardRef } from "preact/compat";
+import { FunctionalComponent, h, RefObject, ComponentChild, JSX, Fragment } from "preact";
+import { forwardRef, useState } from "preact/compat";
 
 interface UniversalEditorProps {
     value: unknown;
-    onChange(value: unknown): void;
-    onRefresh?(): void;
+    valueFormatter?(value: unknown, isLoading: boolean): ComponentChild;
+    onChange(value: unknown): Promise<void>;
+    onRefresh?(): Promise<void>;
     allowEmpty?: boolean;
     titleEdit?: string;
     titleRefresh?: string;
@@ -24,53 +25,77 @@ const togglePairs = new Map<string | boolean, string | boolean>([
 ]);
 */
 
+// eslint-disable-next-line react/display-name
 const UniversalEditor: FunctionalComponent<UniversalEditorProps> = forwardRef((props, ref: RefObject<HTMLInputElement>) => {
     const { value, onChange, onRefresh, allowEmpty, titleEdit, titleRefresh, ...rest } = props;
     /* const isToggleParameter = togglePairs.has(value as string | boolean); */
-	
-    const changeHandler = (event) => {
-        const { target } = event;
-        switch (target.type) {
-            case "checkbox":
-                onChange(target.checked);
-                break;
-            case "number":
-                target.valueAsNumber != value && onChange(target.valueAsNumber);
-                break;
-            default:
-                target.value != value && onChange(target.value);
-                break;
+    const valueFormatter = props.valueFormatter || ((value: unknown): ComponentChild => `${value}`);
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const changeHandler: JSX.GenericEventHandler<HTMLInputElement> = async (event) => {
+        try {
+            setIsLoading(true);
+            const { currentTarget } = event;
+            switch (currentTarget.type) {
+                case "checkbox":
+                    await onChange(currentTarget.checked);
+                    break;
+                case "number":
+                    currentTarget.valueAsNumber != value && await onChange(currentTarget.valueAsNumber);
+                    break;
+                default:
+                    currentTarget.value != value && await onChange(currentTarget.value);
+                    break;
+            }
+        } finally {
+            setIsLoading(false);
         }
     };
+
+    const refreshHandler = async (): Promise<void> => {
+        try {
+            setIsLoading(true);
+            await onRefresh();
+        } finally {
+            setIsLoading(false);
+        }
+    }
 
     const editHandler = async (): Promise<void> => {
         const newVal = prompt("Enter new value", value as string);
         if (newVal !== null && (newVal !== "" || allowEmpty)) {
-          onChange(newVal);
+            try {
+                setIsLoading(true);
+                await onChange(newVal);
+            } finally {
+                setIsLoading(false);
+            }
         }
     };
 
+    let input: ComponentChild;
+
     switch (typeof value) {
         case "boolean":
-            return <div class="d-inline-flex align-items-center">
-                       {onRefresh ? <button type="button" title={titleRefresh} class="btn btn-sm"><i class="fas fa-sync" onClick={onRefresh} /></button> : null }
-                       <input ref={ref} {...rest} type="checkbox" checked={value} onChange={changeHandler} />
-                   </div>;
-        case "number":
-            /* return <input step="any" ref={ref} {...rest} type="number" value={value} onBlur={changeHandler} />; */
-        default:            
-			/*if (isToggleParameter) {
-				return <div class="custom-control custom-switch"><input ref={ref} class="custom-control-input" type="checkbox" onChange={changeHandler}/>
-				<label class="custom-control-label">{value as string}</label>
-				</div>;
-			};*/
-
-            /* return <input ref={ref} {...rest} type="text" value={value as string} onBlur={changeHandler} />; */
-            return <div class="d-inline-flex align-items-baseline">
-                       {onRefresh ? <button type="button" title={titleRefresh} class="btn btn-sm"><i class="fas fa-sync" onClick={onRefresh} /></button> : null }
-                       <button type="button" title={titleEdit} class="btn btn-sm"><i class="fas fa-edit" onClick={editHandler} /></button>
-                       <div>{value as string}</div>
-                   </div>;
+            input = <input ref={ref} {...rest} type="checkbox" checked={value as boolean} onChange={changeHandler} />;
+            break;
+        //case "number":
+        /* input = <input step="any" ref={ref} {...rest} type="number" value={value} onBlur={changeHandler} />; */
+        default:
+            input = <Fragment>
+                <button type="button" title={titleEdit} disabled={isLoading} class="btn btn-sm" onClick={editHandler}><i class="fas fa-edit" /></button>
+                <div class={`ml-1 ${isLoading ? "text-muted" : ""}`}>{valueFormatter(value, isLoading)}</div>
+            </Fragment>;
+            break;
     }
+
+    return <div class="d-inline-flex align-items-center ml-n2">
+        {onRefresh ? <button type="button" title={titleRefresh} disabled={isLoading} class="btn btn-sm" onClick={refreshHandler}><i class="fas fa-sync" /></button> : null}
+        {input}
+        <div class={`spinner-border spinner-border-sm text-primary ml-3 mr-2 my-1 ${!isLoading ? 'd-none' : ''}`} role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>;
 });
 export default UniversalEditor;
